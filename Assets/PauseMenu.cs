df9800ba4fd4a50268c13158c4cc0158aa26b735
Darcy <darcyloanebillings@gmail.com>
2024-03-23 18:09:47 -0400

    Updating Pause Menu

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -4,33 +4,47 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    public bool isPaused;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        gameObject.SetActive(false);
-    }
+    public GameObject pauseMenuUI;
+    public static bool isPaused = false;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-            if (isPaused) ResumeGame();
-            else PauseGame();
+       if(Input.GetKeyDown(KeyCode.Escape))
+       {
+            if(isPaused)
+            {
+            ResumeGame();
+            } 
+            else
+            {
+            PauseGame();
+            }
+       } 
     }
 
     public void PauseGame()
     {
-        gameObject.SetActive(true);
+        pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
     public void ResumeGame()
     {
-        gameObject.SetActive(false);
+        pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+    }
+    // THX BRACKEYS :3!!!
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...")
+    }
+
+    public void QuitGame()
+    {
+        Debug.log("Quitting Game...")
     }
 }
