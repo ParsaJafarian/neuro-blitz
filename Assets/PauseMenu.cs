4248ce4dd539bbc77cd0097dd1e256c7f9771aed
ParsaJafarian <108084316+Parsilius@users.noreply.github.com>
2024-03-23 17:15:46 -0400

    Fix compiler error in PauseMenu.cs

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -4,41 +4,32 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject PauseMenu;
     public bool isPaused;
 
     // Start is called before the first frame update
     void Start()
     {
-        PauseMenu.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetKeyDown(KeyCode.Escape))
-       {
-        if(isPaused)
-        {
-            ResumeGame();
-        }
-        else
-        {
-            PauseGame();
-        }
-       } 
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (isPaused) ResumeGame();
+            else PauseGame();
     }
 
     public void PauseGame()
     {
-        PauseMenu.SetActive(true);
+        gameObject.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
     }
 
     public void ResumeGame()
     {
-        PauseMenu.SetActive(false);
+        gameObject.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
     }
